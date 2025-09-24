f97853597acbe9d83691241e5dd7c28d627b57ea
SabryIR <sabryrafrafi@hotmail.com>
2025-09-24 10:47:47 -0300

    [mod] exercicio tipos

--- tipos/src/main.rs
+++ tipos/src/main.rs
@@ -1,13 +1,13 @@
-// exercicio incompleto
+#![allow(dead_code)]
 
-#[allow(dead_code)]
+#[derive(Debug)]
 enum Semaforo {
     Vermelho,
     Amarelo,
     Verde
 }
 
-#[allow(dead_code)]
+#[derive(Debug)]
 enum Naipe {
     Ouros,
     Copas,
@@ -15,25 +15,93 @@ enum Naipe {
     Espadas
 }
 
-#[allow(dead_code)]
+#[derive(Debug)]
 enum Rank {
     A,
     Numero(u8),
+    Q,
     J,
-    K,
-    Q
+    K
 }
 
-#[allow(dead_code)]
+#[derive(Debug)]
 struct Carta {
     naipe: Naipe,
     rank: Rank
 }
 
-fn dizer_ola() {
-    println!("Olá!");
+#[derive(Debug)]
+struct Ponto {
+    x: f32,
+    y: f32
+}
+
+#[derive(Debug)]
+enum ObjGeometrico {
+    Ponto(Ponto),
+    Semirreta {
+        origem: Ponto,
+        angulo: f32
+    },
+    Segmento(Ponto, Ponto),
+    Quadrilatero([Ponto; 4])
+}
+
+#[derive(Debug)]
+struct Livro {
+    titulo: String,
+    autor: String,
+    paginas: u16
+}
+
+#[derive(Debug)]
+struct Revista {
+    titulo: String,
+    edicao: u8
+}
+
+#[derive(Debug)]
+enum ItemBiblioteca {
+    Livro(Livro),
+    Revista(Revista)
 }
 
 fn main() {
-    dizer_ola();
+    let semaforo = Semaforo::Verde;
+    let as_espadas = Carta {
+        naipe: Naipe::Espadas,
+        rank: Rank::A,
+    };
+    let ponto = ObjGeometrico::Ponto(Ponto {
+        x: 2.0,
+        y: 3.0
+    });
+    let quad = ObjGeometrico::Quadrilatero([
+        Ponto {
+            x: 1.0,
+            y: 1.0,
+        },
+        Ponto {
+            x: 2.0,
+            y: 1.0,
+        },
+        Ponto {
+            x: 1.0,
+            y: 2.0,
+        },
+        Ponto {
+            x: 2.0,
+            y: 2.0,
+        },
+    ]);
+    let livro = ItemBiblioteca::Livro(Livro {
+        titulo: String::from("Crime e Castigo"),
+        autor: String::from("Fiodor Dostoievski"),
+        paginas: 562
+    });
+    println!("{:?}", semaforo);
+    println!("{:?}", as_espadas);
+    println!("{:?}", ponto);
+    println!("{:?}", quad);
+    println!("{:?}", livro);
 }
